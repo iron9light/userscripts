2cc6dd75a3a934b65990952d6dc1d4b6ce0ea2f3
iron9light <iron9light@gmail.com>
2014-11-22 05:26:26 +0800

    Add @downloadURL for donkey4uEd2kGen.user.js

--- donkey4uEd2kGen.user.js
+++ donkey4uEd2kGen.user.js
@@ -7,6 +7,7 @@
 // @match      http://donkey4u.com/detail/*
 // @copyright  2014+, You
 // @updateURL  https://github.com/iron9light/userscripts/raw/master/donkey4uEd2kGen.user.js
+// @downloadURL  https://github.com/iron9light/userscripts/raw/master/donkey4uEd2kGen.user.js
 // ==/UserScript==
 ;
 var elem, getVal, hash, hashPattern, link, linkText, name, namePattern, s, size, sizePattern, strings;
